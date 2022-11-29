401f86c17d42d3f076f03b19e6036edf7a67eb62
Durgavino <114535881+Durgavino@users.noreply.github.com>
2022-11-28 23:40:00 -0400

    textarea change

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
 
 
+
 const d = new Date();
 //const time = d.getHours();
 
 
-const time= d.getHours() %12 || 12;
+const time = d.getHours() % 12 || 12;
 
 
 
@@ -14,18 +15,20 @@ var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
 const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
 ];
 
-const dates = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
+//const dates = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
 
 var currentday = document.getElementById("currentDay");
 
 
 
 let day = weekday[d.getDay()];
-let date = dates[d.getDate()];
+
+
+let date = d.getDate();
 
 const months = monthNames[d.getMonth()];
 
-currentday.innerHTML = day + "," + months + " " + date; 
+currentday.innerHTML = day + "," + months + " " + date;
 
 var rowlist = document.querySelectorAll(".row");
 
@@ -38,73 +41,62 @@ var index = 0;
 function timeblock() {
 
   for (index = 0; index < rowlistarr.length; index++) {
-    console.log("time",time);
-
-
+    console.log("time", time);
     console.log(rowlistarr.length);
+
+
+
     if (time == rowlistarr.length) {
-   
-  
-       document.getElementById(`textarea${index+9}`).style.backgroundColor='red';
-
-    }
-      
-    else if (time < rowlistarr.length) {
- 
-   
-
-document.getElementById(`textarea${index+9}`).style.backgroundColor='green';
-
-    }
-    else if (time > rowlistarr.length) {
+      //document.getElementById(`textarea${index + 9}`).style.backgroundColor = 'red';
     
+      document.getElementById(`${index + 9}`).style.backgroundColor = 'red';
+    }
 
-document.getElementById(`textarea${index+9}`).style.backgroundColor='gray';
+    else if (time < rowlistarr.length) {
+     // document.getElementById(`textarea${index + 9}`).style.backgroundColor = 'green';
+     document.getElementById(`${index + 9}`).style.backgroundColor = 'green';
+    }
+  
 
+    else 
+     {
+      //document.getElementById(`textarea${index + 9}`).style.backgroundColor = 'gray';
+      document.getElementById(`${index + 9} `).style.backgroundColor = 'grey';
+    
     }
 
   }
-
 }
 timeblock();
 
 
 
+var sbtn = document.getElementById('sbtn');
 
 
-function save()
-{
+sbtn.addEventListener('click', save);
 
-var storedvalue=localStorage.getItem('textarea9');
+function save() {
 
+  var storedvalue = localStorage.getItem('textarea9');
+  var textarea9 = document.getElementById('textarea9').value;
 
-var textarea9=document.getElementById('textarea9').value;
- 
-  
-if(storedvalue){
-  storedvalue.push(textarea9);
-  
-}
-else{
-  storedvalue=[textarea9];
-}
-  localStorage.setItem('text',JSON.stringify(storedvalue));
-
+  if (storedvalue) {
+    storedvalue.push(textarea9);
+  }
+  else {
+    storedvalue = [textarea9];
+  }
+  localStorage.setItem('text', JSON.stringify(storedvalue));
 }
 
-
-function get()
-{
-  
-var storedvalue=localStorage.getItem('textarea9');
-
-
-if (storedvalue)
-{
-document.getElementById('textarea9').value=storedvalue;
-  
-
-}
+function get() {
+  var storedvalue = localStorage.getItem('text');
+  if (storedvalue) {
+    document.getElementById('textarea9').value = storedvalue;
+  }
+  localStorage.getItem('text', JSON.parse(storedvalue));
 }
 
-location.reload(true);
+get();
+
